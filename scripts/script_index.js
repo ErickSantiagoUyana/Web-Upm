e4423cf21b23c3fce13e3a5a3bf3e4fe64e79f19
santi <santi@klevers-MacBook-Pro.local>
2021-04-19 14:46:30 +0200

    part 4

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,12 +1,12 @@
 
-function main(){
+function main() {
 	create_localStore_index();
 	create_index();
 	create_user();
 
 }
 
-function create_user(){
+function create_user() {
 	let dates = JSON.parse(window.localStorage.getItem('dates'));
 	if (dates == null) {
 		let dates = {
@@ -35,7 +35,6 @@ function getUser(dates, name, password) {
 	}
 	return null;
 }
-
 
 function log(int) {
 	if (int == true) {
@@ -66,27 +65,29 @@ function validation() {
 		name.value = '';
 		password.value = '';
 	} else {
-		if (user.type == 'writer'){
+		if (user.type == 'reader') {
 			dates.sesion = true;
 			window.localStorage.setItem('dates', JSON.stringify(dates));
 			log(true);
+			create_index();
+			location.reload();
+
 		}
 	}
 	return user != null;
 }
 
-
 function create_localStore_index() {
 	if (JSON.parse(window.localStorage.getItem('bdd')) == null) {
 		let bdd = {
 			product: [
-				{ key: 0, name: 'HTML', birth: '1993', death: '', wiki: 'https://en.wikipedia.org/wiki/HTML', img: './images/js_logo.png' },
+				{ key: 0, name: 'HTML', birth: '1993', death: '', wiki: 'https://en.wikipedia.org/wiki/HTML', img: '../images/js_logo.png' },
 			],
 			entity: [
-				{ key: 0, name: 'WHATWG', birth: '2004', death: '', wiki: 'https://en.wikipedia.org/wiki/WHATWG', img: './images/js_logo.png' },
+				{ key: 0, name: 'WHATWG', birth: '2004', death: '', wiki: 'https://en.wikipedia.org/wiki/WHATWG', img: '../images/js_logo.png' },
 			],
 			person: [
-				{ key: 0, name: 'Tim Berners-Lee', birth: '1955', death: '', wiki: 'https://en.wikipedia.org/wiki/Tim_Berners-Lee', img: './images/js_logo.png'},
+				{ key: 0, name: 'Tim Berners-Lee', birth: '1955', death: '', wiki: 'https://en.wikipedia.org/wiki/Tim_Berners-Lee', img: '../images/js_logo.png' },
 			],
 			relation: [
 				{ pkey: 0, ekey: 0, pkey: 0 }
@@ -96,17 +97,17 @@ function create_localStore_index() {
 	}
 }
 
-function create_index(){
+function create_index() {
 
 	let entity = document.getElementById('main_index')
 	let bdd = JSON.parse(window.localStorage.getItem('bdd'));
 
-	entity.appendChild(create_container(bdd.product, 'Productos'));
-	entity.appendChild(create_container(bdd.person, 'Personas'));
-	entity.appendChild(create_container(bdd.entity, 'Entidades'));
+	entity.appendChild(create_container(bdd.product, 'Productos', 'product'));
+	entity.appendChild(create_container(bdd.person, 'Personas', 'person'));
+	entity.appendChild(create_container(bdd.entity, 'Entidades', 'entity'));
 }
 
-function create_container(bdd, name_container) {
+function create_container(bdd, name_container, type_list) {
 	let container_info = document.createElement('div');
 	let name = document.createElement('h2');
 	let type_info = document.createElement('div');
@@ -119,23 +120,25 @@ function create_container(bdd, name_container) {
 		container_info.appendChild(name);
 		type_info.className = 'type_info';
 		for (info of bdd) {
-			type_info.appendChild(create_card(info.name, info.img));
+			type_info.appendChild(create_card(info.name, info.img, info.key, type_list,));
 		}
 		container_info.appendChild(type_info);
 	}
 	return container_info;
 }
 
-function create_card(name_info, img_src) {
+function create_card(name_info, img_src, key_info, type_list) {
 	let type = document.createElement('div');
 	let a = document.createElement('a');
 	let img = document.createElement('img')
 	let name = document.createElement('h2');
+	let dates = JSON.parse(window.localStorage.getItem('dates'));
 
 	type.className = 'type';
-	img.setAttribute('oneclick', 'alert(33)');
-
-	//a.setAttribute('href', './html/details.html');
+	if(dates.sesion == true)
+	{
+		a.setAttribute('onclick', 'save_details("' + String(type_list) + '","' + String(key_info) + '")');
+	}
 	img.className = 'img';
 	img.src = img_src;
 	name.textContent = name_info
@@ -146,9 +149,62 @@ function create_card(name_info, img_src) {
 	return type;
 }
 
-function prueba()
-{
-	alert('sasas');
-
+function save_details(list, key) {
+	let info_details = {
+		type_list: list,
+		key_info: key,
+	}
+	window.localStorage.setItem('info_details', JSON.stringify(info_details));
+	window.location.href = "./html/details.html";
 }
 
+function create_details() {
+	let info_details = JSON.parse(window.localStorage.getItem('info_details'));
+
+	let bdd = get_details(info_details.type_list, info_details.key_info);
+
+	let name_details = document.getElementById('name_details');
+	let img = document.getElementById('img');
+
+	name_details.textContent = bdd.name;
+	img.src = bdd.img;
+}
+
+function get_details(list, key) {
+	let bdd = JSON.parse(window.localStorage.getItem('bdd'));
+
+	if(list == 'product'){
+		for (bdd of bdd.product) {
+			if (bdd.key == key) {
+				return bdd;
+			}
+		}
+	}
+	if(list == 'person'){
+		for (bdd of bdd.person) {
+			if (bdd.key == key) {
+				return bdd;
+			}
+		}
+	}
+	if(list == 'entity'){
+		for (bdd of bdd.entity) {
+			if (bdd.key == key) {
+				return bdd;
+			}
+		}
+	}
+
+	/*
+		if(list == 'product')
+		{
+			for (user of dates.users) {
+				if (user.name == name &&
+					user.password == password) {
+					return user;
+				}
+			}
+		}
+	*/
+
+}
